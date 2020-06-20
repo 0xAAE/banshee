c33b49a2bcd3a1f09b04cafe4bd9123ee8112a0a
aav@credits.com <aav@credits.com>
2020-06-20 12:24:19 +0300

    Add support for ctrl-c stop

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ mod output;
 mod config;
 use config::Config;
 
-//use tokio::prelude::*;
-use log::{info, debug};
+use log::info;
+use signal_hook::{iterator::Signals, SIGINT};
 
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -20,29 +20,36 @@ type StopFlag = Arc<AtomicBool>;
 #[tokio::main]
 async fn main() {
 
+    println!("Hello, banshee");
+
     // instantiate config
     let cfg_inst = Config::new();
 
     // init logger
     logger::init(cfg_inst.clone());
     // now logging is available
-    info!("Hello, banshee");
 
     let stop_flag: StopFlag = Arc::new(AtomicBool::new(false));
 
-    let submodules = async move {
+    let subsystems = async move {
         input::run(cfg_inst.clone(), stop_flag.clone()).await;
         collector::run(cfg_inst.clone(), stop_flag.clone()).await;
         processor::run(cfg_inst.clone(), stop_flag.clone()).await;
         inference::run(cfg_inst.clone(), stop_flag.clone()).await;
         output::run(cfg_inst.clone(), stop_flag.clone()).await;
 
-        // imitate 5 sec work:
-        thread::sleep(time::Duration::from_secs(15));
-        stop_flag.store(true, Ordering::SeqCst);
+        let signals = Signals::new(&[SIGINT]).unwrap();
+        tokio::spawn(async move {
+            for sig in signals.forever() {
+                println!("Received signal {:?}", sig);
+                stop_flag.store(true, Ordering::SeqCst);
+                println!("Trying to stop banshee!");
+                break;
+            }
+        }).await.unwrap();
     };
 
-    submodules.await;
+    subsystems.await;
 
-    info!("exit main");
+    println!("Banshee has started");
 }
